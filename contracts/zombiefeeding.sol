3dc37e447cd6f4d5b50765ec341d283152cdb84c
Daniyl Matveyw <daniylmatveyw@Daniyls-MacBook-Air.local>
2025-10-12 17:59:12 +0300

    lil work with timestamp

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -22,8 +22,16 @@ contract ZombieFeeding is ZombieFactory {
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
   KittyInterface kittyContract;
 
-  function setKittyContractAddress(address _address) external {
+  function setKittyContractAddress(address _address) external onlyOwner {
     kittyContract = KittyInterface(_address);
+  }
+
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(block.timestamp + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+    return (_zombie.readyTime <= block.timestamp);
   }
 
   function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
